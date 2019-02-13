95a663c1e60c5d7609c35d16c2144214396d4b78
Ian <isalmon1@gmail.com>
2019-02-12 17:06:58 -0700

    Update and delete functionality full. Styling text for large view and submit button.

--- app/app.js
+++ app/app.js
@@ -17,7 +17,9 @@ $(document).ready(function(){
   var createDropdownButtons = function() {
     $('.dropdown-content').html('');
     Object.keys(localStorage).forEach(key => {
-      $('.dropdown-content').append('<button class="btn-dropdown-name">' + key + '</button>&nbsp&nbsp');
+      if (key !== 'temp') {
+        $('.dropdown-content').append('<button class="btn-dropdown-name">' + key + '</button>&nbsp&nbsp');
+      }
     })
   }
 
@@ -44,7 +46,9 @@ $(document).ready(function(){
     $('.display-large-view').append('<div class="container-large-view-btns"><br><button class="btn-update">Update</button>&nbsp<button class="btn-delete">Delete</button></div><br>');
     $('.display-large-view').append('<div class="container-large-view-data"></div>');
     Object.keys(displayText).forEach(key => {
-      $('.container-large-view-data').append('<div class="display-data-item">' + key + JSON.stringify(displayText[key]) + '</div>');
+      $('.container-large-view-data').append('<div class="display-data-item">' + key + '</div>');
+      $('.container-large-view-data').append('<p class="display-data-details"><i>Instrument:</i>&nbsp' + displayText[key]["instrument"] + ',&nbsp&nbsp<i>Tonalization:</i>&nbsp' + displayText[key]["tonalization"] +',&nbsp&nbsp<i>Scales:</i>&nbsp' + displayText[key]["scales"] + ',&nbsp&nbsp<i>Etude:</i>&nbsp' + displayText[key]["etude"] +',&nbsp&nbsp<i>Review Pieces:</i>&nbsp' + displayText[key]["review pieces"] + ',&nbsp&nbsp<i>Working Piece:</i>&nbsp' + displayText[key]["working piece"] +',&nbsp&nbsp<i>Reading:</i>&nbsp' + displayText[key]["reading"]);
+
     })
   }
 
@@ -70,14 +74,13 @@ $(document).ready(function(){
     $('.display-large-view').append('<div class="container-large-view-data"></div>');
     Object.keys(displayText).forEach(key => {
       $('.container-large-view-data').append('<div class="display-data-item"><button class="btn-delete-item">Delete</button>&nbsp' + key + '&nbsp&nbsp</div>');
-      $('.container-large-view-data').append('<p class="display-data-details"><i>Instrument:</i>&nbsp' + displayText[key]["instrument"] + ',&nbsp&nbsp<i>Tonalization:</i>&nbsp' + displayText[key]["tonalization"] +',&nbsp&nbsp<i>Scales:</i>&nbsp' + displayText[key]["scales"] + ',&nbsp&nbsp<i>Etude:</i>&nbsp' + displayText[key]["etude"] +',&nbsp&nbsp<i>Review Pieces:</i>&nbsp' + displayText[key]["review pieces"] + ',&nbsp&nbsp<i>Working Piece:</i>&nbsp' + displayText[key]["working piece"] +',&nbsp&nbsp<i>Reading:</i>&nbsp' + displayText[key]["reading"])
-      console.log(displayText[key]["instrument"])
+      $('.container-large-view-data').append('<p class="display-data-details"><i>Instrument:</i>&nbsp' + displayText[key]["instrument"] + ',&nbsp&nbsp<i>Tonalization:</i>&nbsp' + displayText[key]["tonalization"] +',&nbsp&nbsp<i>Scales:</i>&nbsp' + displayText[key]["scales"] + ',&nbsp&nbsp<i>Etude:</i>&nbsp' + displayText[key]["etude"] +',&nbsp&nbsp<i>Review Pieces:</i>&nbsp' + displayText[key]["review pieces"] + ',&nbsp&nbsp<i>Working Piece:</i>&nbsp' + displayText[key]["working piece"] +',&nbsp&nbsp<i>Reading:</i>&nbsp' + displayText[key]["reading"]);
     })
     $('.container-large-view-data').append('<div><br><button class="btn-return-to-large-view">Cancel</button>');
   }
 
   var createLargeViewUpdateBtns = function(event) {
-      var keyData = $('.display-data-name').text();
+    var keyData = $('.display-data-name').text();
     var displayText = JSON.parse(localStorage.getItem(keyData));
 
     $('.container-workspace').html('');
@@ -89,7 +92,9 @@ $(document).ready(function(){
     $('.notes-data').append('<div class="display-data-name">Student:&nbsp' + keyData + '</div><br>');
 
     Object.keys(displayText).forEach(key => {
-      $('.container-large-view-data').append('<div class="display-data-item"><button class="btn-update-item">Update</button>&nbsp' + key + '&nbsp&nbsp' + JSON.stringify(displayText[key]) + '</div>');
+      $('.container-large-view-data').append('<div class="display-data-item"><button class="btn-update-item">Update</button>&nbsp' + key + '&nbsp&nbsp</div>');
+      $('.container-large-view-data').append('<p class="display-data-details"><i>Instrument:</i>&nbsp' + displayText[key]["instrument"] + ',&nbsp&nbsp<i>Tonalization:</i>&nbsp' + displayText[key]["tonalization"] +',&nbsp&nbsp<i>Scales:</i>&nbsp' + displayText[key]["scales"] + ',&nbsp&nbsp<i>Etude:</i>&nbsp' + displayText[key]["etude"] +',&nbsp&nbsp<i>Review Pieces:</i>&nbsp' + displayText[key]["review pieces"] + ',&nbsp&nbsp<i>Working Piece:</i>&nbsp' + displayText[key]["working piece"] +',&nbsp&nbsp<i>Reading:</i>&nbsp' + displayText[key]["reading"])
+
     })
     $('.container-large-view-data').append('<div><br><button class="btn-return-to-large-view">Cancel</button>');
   }
@@ -157,6 +162,38 @@ $(document).ready(function(){
     localStorage.setItem('temp', "{}");
   }
 
+  var writeNoteToView = function() {
+    var name = $('.input-name').val();
+    var date = $('.input-date').val();
+    var tempObj = JSON.parse(localStorage[name]);
+
+    var instrument = tempObj[date]["instrument"];
+    var tonalization = tempObj[date]["tonalization"];
+    var scales = tempObj[date]["scales"];
+    var etude = tempObj[date]["etude"];
+    var reviewPieces = tempObj[date]["review pieces"];
+    var workingPiece = tempObj[date]["working piece"];
+    var reading = tempObj[date]["reading"];
+
+    $('.notes-data').text('');
+    $('.notes-data').append('<div class="temp-name">Name:&nbsp</div><div class="temp-date">Date:&nbsp</div><div class="temp-instrument">Instrument:&nbsp</div><br><div class="temp-tonalization">Tonalization:</div><br><div class="temp-scales">Scales:</div><br><div class="temp-etude">Etude:</div><br><div class="temp-review">Review&nbspPieces:</div><br><div class="temp-working-piece">Working&nbspPiece:</div><br><div class="temp-reading">Reading:</div>');
+
+
+    $('.temp-name').append(name);
+    $('.temp-date').append(date);
+    $('.temp-instrument').append(instrument);
+    $('.temp-tonalization').append('<div class="temp-notes">' + tonalization + '</div>');
+    $('.temp-scales').append('<div class="temp-notes">' + scales + '</div>');
+    $('.temp-etude').append('<div class="temp-notes">' + etude + '</div>');
+    $('.temp-review').append('<div class="temp-notes">' + reviewPieces + '</div>');
+    $('.temp-working-piece').append('<div class="temp-notes">' + workingPiece + '</div>');
+    $('.temp-reading').append('<div class="temp-notes">' + reading + '</div>');
+
+
+    $('.notes-data').append('<br><div class="submission-area"></div>');
+    $('.submission-area').append('<div class="warning-submit">WARNING: Note is not fully saved until you click "submit"!</div><button class="btn-add">Submit</button>');
+  }
+
   var writeTempToView = function() {
     // $('.notes-data').append(localStorage.temp);
     var name = $('.input-name').val();
@@ -179,10 +216,13 @@ $(document).ready(function(){
     // console.log(reviewPieces);
     // console.log(workingPiece);
     // console.log(reading);
+    $('.notes-data').text('');
+    $('.notes-data').append('<div class="temp-name">Name:&nbsp</div><div class="temp-date">Date:&nbsp</div><div class="temp-instrument">Instrument:&nbsp</div><br><div class="temp-tonalization">Tonalization:</div><br><div class="temp-scales">Scales:</div><br><div class="temp-etude">Etude:</div><br><div class="temp-review">Review&nbspPieces:</div><br><div class="temp-working-piece">Working&nbspPiece:</div><br><div class="temp-reading">Reading:</div>');
+  
 
-    $('.temp-name').append(name);
-    $('.temp-date').append(date);
-    $('.temp-instrument').append(instrument);
+    $('.temp-name').append('<span class="inline-notes">' + name + '</span>');
+    $('.temp-date').append('<span class="inline-notes">' + date + '</span>');
+    $('.temp-instrument').append('<span class="inline-notes">' + instrument + '</span>');
     $('.temp-tonalization').append('<div class="temp-notes">' + tonalization + '</div>');
     $('.temp-scales').append('<div class="temp-notes">' + scales + '</div>');
     $('.temp-etude').append('<div class="temp-notes">' + etude + '</div>');
@@ -191,8 +231,8 @@ $(document).ready(function(){
     $('.temp-reading').append('<div class="temp-notes">' + reading + '</div>');
 
 
-    $('.notes-data').append('<div class="submission-area"></div>');
-    $('.submission-area').append('<button class="btn-add">Submit</button>');
+    $('.notes-data').append('<br><div class="submission-area"></div>');
+    $('.submission-area').append('WARNING: Note is not fully saved until you click "submit"!<button class="btn-add">Submit</button>');
   }
 
   $(".btn-take-notes").on('click', function() {
@@ -202,9 +242,10 @@ $(document).ready(function(){
     createInstructionsView();
     addDividerBegin();
     createLessonForm();
-    $('.notes-data').text('');
+    $('.notes-data').html('');
     $('.notes-data').append('<div class="temp-name">Name:&nbsp</div><div class="temp-date">Date:&nbsp</div><div class="temp-instrument">Instrument:&nbsp</div><br><div class="temp-tonalization">Tonalization:</div><br><div class="temp-scales">Scales:</div><br><div class="temp-etude">Etude:</div><br><div class="temp-review">Review&nbspPieces:</div><br><div class="temp-working-piece">Working&nbspPiece:</div><br><div class="temp-reading">Reading:</div>');
-  });
+
+});
 
   $(".btn-view-student").on('click', function() {
     if ($(".container-select-student").css("visibility") === "hidden") {
@@ -261,6 +302,7 @@ $(document).ready(function(){
     $('.notes-data').html('');
     addDividerBegin();
     createUpdateForm(name, date);
+    writeNoteToView();
   })
 
   var createUpdateForm = function() {
